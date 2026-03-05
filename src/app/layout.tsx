1e871c8fec0a7832212b5e3423d8d82051603b0b
Kai Itokazu <56910039+kitokazu@users.noreply.github.com>
2026-03-05 15:43:26 +0900

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,6 +47,11 @@ export default function RootLayout({
   return (
     <html lang="ja" className="scroll-smooth">
       <head>
+        <link rel="icon" type="image/png" href="/favicon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
         <link
           href="https://fonts.googleapis.com/css2?family=Shippori+Mincho:wght@400;500;600;700&display=swap"
           rel="stylesheet"
